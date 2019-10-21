33bdddbb1e7e32b0ee52b11f1e335bdffb823795
Jan Häusler <jan.haeusler@rwth-aachen.de>
2019-10-21 19:43:22 +0200

    Implemented log likelihood estimation.

--- exercise-01/q6_expectation_maximization_python/getLogLikelihood.py
+++ exercise-01/q6_expectation_maximization_python/getLogLikelihood.py
@@ -1,4 +1,23 @@
 import numpy as np
+
+def multiDimensionalNormal(mean, covariance, value):
+    D = len(value)
+
+    normalizationFactor = 1/((2*np.pi)**(D/2)*np.linalg.det(covariance)**(0.5))
+
+    distribution = np.exp(-0.5 * (np.transpose(value-mean) @ np.linalg.inv(covariance) @ (value-mean)))
+
+    return normalizationFactor*distribution
+
+def getLogEntry(means, weights, covariances, value):
+    K = means.shape[0]
+    
+    innerSum = 0
+    for k in range(K):
+        innerSum += weights[k]*multiDimensionalNormal(means[k], covariances[:,:,k], value)
+
+    return np.log(innerSum)
+
 def getLogLikelihood(means, weights, covariances, X):
     # Log Likelihood estimation
     #
@@ -14,6 +33,12 @@ def getLogLikelihood(means, weights, covariances, X):
     # OUTPUT:
     # logLikelihood  : log-likelihood
 
-    #####Insert your code here for subtask 6a#####
-    return logLikelihood
+    means = np.array(means)
+    weights = np.array(weights)
+
+    sumOverDataPoints = 0
+    for n in range(X.shape[0]):
+        sumOverDataPoints += getLogEntry(means, weights, covariances, X[n])
+
+    return sumOverDataPoints
 
